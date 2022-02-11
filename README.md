6db4ff93b103227fa5a1a5e125ce33ce5cf5a488
L1keMagic <karachev.artur@gmail.com>
2022-02-11 13:34:19 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# WPF Password box
+
+<img src="src/hiden.png" alt="демо">
+<img src="src/shown.png" alt="демо">
+
+Here is updated version of "Display Password in PasswordBox in WPF"
+
+Inspired by https://codescratcher.com/wpf/display-password-passwordbox-wpf/
---
+++ TestShowHidePSBX/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿namespace TestShowHidePSBX
+{
+    using System;
+    using System.Windows;
+    using System.IO;
+    using System.Windows.Input;
+    using System.Windows.Media.Imaging;
+
+    public partial class MainWindow : Window
+    {
+
+        private readonly BitmapImage showImage = new BitmapImage(
+            new Uri(Path.Combine("/", "Resources", "Show.jpg"), UriKind.Relative));
+        private readonly BitmapImage hideImage = new BitmapImage(
+            new Uri(Path.Combine("/", "Resources", "Hide.jpg"), UriKind.Relative));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ShowPassword();
+        }
+
+        private void OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            HidePassword();
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            HidePassword();
+        }
+
+        private void ShowPassword()
+        {
+            ImgShowHide.Source = hideImage;
+            txtVisiblePasswordbox.Visibility = Visibility.Visible;
+            txtPasswordbox.Visibility = Visibility.Collapsed;
+            txtVisiblePasswordbox.Text = txtPasswordbox.Password;
+        }
+
+        private void HidePassword()
+        {
+            ImgShowHide.Source = showImage;
+            txtVisiblePasswordbox.Visibility = Visibility.Collapsed;
+            txtPasswordbox.Visibility = Visibility.Visible;
+            txtPasswordbox.Focus();
+        }
+
+        private void txtPasswordbox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (txtPasswordbox.Password.Length > 0)
+            {
+                ImgShowHide.Visibility = Visibility.Visible;
+                HidePassword();
+            }
+            else
+            {
+                ImgShowHide.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
